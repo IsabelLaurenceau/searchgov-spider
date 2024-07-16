a4485861fe7575ebb355a71f361f3cf0341c3812
Isabel <108532577+IsabelLaurenceau@users.noreply.github.com>
2024-07-16 14:24:24 -0700

    Delete pylint.yml
    Update pylint.yml
    
    Create pylint.yml
    
    Update pylint.yml
    
    PyLint
    
    Create pylint.yml

--- search_gov_crawler/search_gov_spiders/spiders/domain_spider.py
+++ search_gov_crawler/search_gov_spiders/spiders/domain_spider.py
@@ -1,29 +1,43 @@
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+"""_summary_
+
+Yields:
+    _type_: _description_
+"""
+
 import os
 import sys
 
-starting_urls = os.path.join(os.path.dirname(sys.modules[__name__].__file__),"../utility_files/startingUrls.txt")
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+starting_urls = os.path.join(
+    os.path.dirname(sys.modules[__name__].__file__), "../utility_files/startingUrls.txt"
+)
 
 start_urls_list = []
-with open(starting_urls) as file:
+with open(starting_urls, encoding="utf-8") as file:
     while line := file.readline():
         start_urls_list.append(line.rstrip())
 
-domains = os.path.join(os.path.dirname(sys.modules[__name__].__file__),"../utility_files/domains.txt")
+domains = os.path.join(
+    os.path.dirname(sys.modules[__name__].__file__), "../utility_files/domains.txt"
+)
 
 domains_list = []
-with open(domains) as file:
+with open(domains, encoding="utf-8") as file:
     while line := file.readline():
         domains_list.append(line.rstrip())
 
 
-# scrapy command for crawling domain/site
-# scrapy crawl domain_spider -a domain=desired_domain -a urls=desired_url
-# ex: scrapy crawl domain_spider -a domain=travel.dod.mil -a urls=https://travel.dod.mil
-
-
 class DomainSpider(CrawlSpider):
+    """
+    The DomainSpider is able to crawl domains specified in the arguments.
+
+    scrapy command for crawling domain/site
+    scrapy crawl domain_spider -a domain=desired_domain -a urls=desired_url
+    ex: scrapy crawl domain_spider -a domain=travel.dod.mil -a urls=https://travel.dod.mil
+    """
+
     name = "domain_spider"
 
     def __init__(self, domain=None, urls=None, *args, **kwargs):
@@ -31,16 +45,17 @@ class DomainSpider(CrawlSpider):
         # will grab singular domain
         # multiple comma-separated inputs (ex input: domain=getsmartaboutdrugs.gov,travel.dod.mil)
         # or the list of search.gov domains
-        if domain and ',' in domain:
-            self.allowed_domains = domain.split(',')
+        if domain and "," in domain:
+            self.allowed_domains = domain.split(",")
         else:
             self.allowed_domains = [domain] if domain else domains_list
         # urls to start crawling from in domain(s)
         # will grab singular start url
-        # multiple comma-separated inputs (ex input: urls=https://getsmartaboutdrugs.gov,https://travel.dod.mil)
+        # multiple comma-separated inputs
+        # (ex input: urls=https://getsmartaboutdrugs.gov,https://travel.dod.mil)
         # or the list of search.gov start urls
-        if urls and ',' in urls:
-            self.start_urls = urls.split(',')
+        if urls and "," in urls:
+            self.start_urls = urls.split(",")
         else:
             self.start_urls = [urls] if urls else start_urls_list
 
